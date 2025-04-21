6aac1bdae66d9c9dc05aaabc934304c85402ac33
11akriti11 <akriti1220.be22@chitkara.edu.in>
2025-04-21 18:28:55 +0530

    ak

--- frontend/src/pages/AdminPanel/AdminPanel.jsx
+++ frontend/src/pages/AdminPanel/AdminPanel.jsx
@@ -2,37 +2,16 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 
 const AdminPanel = () => {
-  // return (
-  //   <div className="min-h-screen bg-gray-100 font-sans flex flex-col items-center pt-20">
-  //     <div className="w-full max-w-4xl p-8 bg-white shadow-lg rounded-lg">
-  //       <h1 className="text-4xl font-bold text-gray-800 text-center mb-8">
-  //         Admin Panel
-  //       </h1>
-  //       <nav className="flex justify-center gap-6 mb-8">
-  //         <Link
-  //           to="pets"
-  //           className="px-6 py-3 bg-blue-500 text-white font-medium rounded-lg hover:bg-blue-600 transition-all shadow-md"
-  //         >
-  //           Pets
-  //         </Link>
-  //         <Link
-  //           to="users"
-  //           className="px-6 py-3 bg-green-500 text-white font-medium rounded-lg hover:bg-green-600 transition-all shadow-md"
-  //         >
-  //           Users
-  //         </Link>
-  //       </nav>
-  //       <div className="p-4">
-  //         <Outlet />
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
   return(
     <div>
-      <h1>This is the Admin Dashboard and is currently in progress.</h1>
-    </div>
+  <img 
+    src="../../assets/admin.png"
+    alt="Admin Dashboard" 
+    style={{ width: '100vw', height: '100vh', marginBottom: '20px' }} 
+  />
+  {/* <h1>This is the Admin Dashboard and is currently in progress.</h1> */}
+</div>
+
   );
 
 };
