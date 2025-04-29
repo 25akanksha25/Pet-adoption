458ceff2185df1e423d7c37ed7c294b7dcadd710
Akanksha Chopra <chopraakanksha10@gmail.com>
2025-04-29 17:06:14 +0530

    final ak

--- backend/config/razorpayPayement.js
+++ backend/config/razorpayPayement.js
@@ -1,7 +1,8 @@
 import Razorpay from 'razorpay';
 
-const api_key="rzp_test_HA0dblFvRuSZ2X";
-const api_Secret_key="WnueYlnYvG1GD7pVoQZv9OzE";
+const api_key="rzp_test_5vn2DOA5ooekAb";
+const api_Secret_key="sFLn8FhT65rd4izWpWRmSb5e";
+
 
 export const razorpay = new Razorpay({
   key_id:api_key,
